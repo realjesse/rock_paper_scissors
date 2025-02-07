c2992ca7a33c8f0048c8ccf8f45c12323e0a8f54
realjesse <154276383+realjesse@users.noreply.github.com>
2025-02-07 14:09:54 -0800

    Create basic psuedocode

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("Hello World!");
+/*
+Play a game, with 5 rounds
+Get computer choice
+Get player input
+Keep score
+Play a round
+IF player win:
+    increase score
+    write a message
+ELSE:
+    Increase bot score
+    write a lose message
+
+Start game
+    SET bot score to 0
+    SET player score to 0
+    FOR 5 times:
+        Start round
+            COMPUTE bot decision
+            GET player decision
+            DETERMINE winner
+                IF player win:
+                    INCREMEMENT player score
+                    PRINT winning message
+                ELSE:
+                    INCREMENT bot score
+                    PRINT losing message
+*/
