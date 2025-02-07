a1c11a6e1bc3e44a8c66c08ac7cc4de132c0be2c
realjesse <154276383+realjesse@users.noreply.github.com>
2025-02-07 15:42:35 -0800

    Create getComputerChoice()

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function getRandomInt() {
+    /* Will return a random int between the values of 0 and 2 */
     return Math.floor(Math.random() * 3);
+}
+
+function getComputerChoice() {
+    var randomInt = getRandomInt();
+
+    if (randomInt === 1) {
+        return "rock";
+    }
+
+    else if (randomInt === 2) {
+        return "paper";
+    }
+
+    else {
+        return "scissors";
+    }
 }
 
 /*
